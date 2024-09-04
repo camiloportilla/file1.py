9f0ef3e66aa41767db8340deb9029fc8f6114d78
camiloportilla <kamilorosero11@gmail.com>
2024-09-04 15:53:43 -0500

    6ta modificacion, aprendiendo busquedas con index, numerate y append

---
+++ busqueda.py
@@ -0,0 +1,13 @@
+print("en que posicion esta la caja")
+cinta = ["vacio", "vacio", "vacio", "caja", "vacio", "vacio", "caja", "vacio", "caja", "vacio", ]
+posicion = cinta.index("caja")
+print("aqui esta la caja: ", posicion)
+posiciones = [i for i, valor in enumerate(cinta) if valor == "caja"]
+
+print("Las cajas están en las posiciones:", posiciones)
+
+posicionesx = []
+for i, valor in enumerate(cinta):
+    if valor == "caja":
+        posicionesx.append(i)
+print("Las cajas están en las posiciones:", posicionesx)
